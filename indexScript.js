d42263a489b1d9c13d40caef2c44d72b91f0c468
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-09-15 18:59:54 -0400

    initial commit

---
+++ indexScript.js
@@ -0,0 +1,31 @@
+const countdown = document.getElementById("countdown");
+const bodyContainer = document.getElementById("bodyContainer");
+const redirectTextTime = document.getElementById("redirectTextTime");
+const init = parseInt(countdown.textContent);
+const spin = document.getElementById("spinner");
+
+var n = Math.round(init);
+
+function start() {
+	let timer = setInterval(() => {
+		n -= 1;
+		update();
+		if (n === 0) {
+ 			clearInterval(timer);
+			spin.classList.remove("hidden");
+			bodyContainer.classList.add("hidden");
+			window.location.href = '/pages/landing/landing.html';
+			
+		}
+	}, 1000);
+}
+
+function update() {
+	countdown.textContent = `${n}`;
+	redirectTextTime.textContent = `${Math.round(init)}`;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	update();
+	start();
+});
---
+++ pages/landing/landingScript.js
@@ -0,0 +1,30 @@
+var clock = 0;
+
+const spin = document.getElementById("spinner");
+const bodyContainer = document.getElementById("bodyContainer");
+const landingText = document.getElementById("landingText");
+const landingTextBg = document.getElementById("landingTextBg");
+
+function loadIn(){
+	setTimeout(() => {
+		spin.classList.add("hidden");
+		bodyContainer.classList.remove("hidden");
+		landingText.classList.remove("hidden");
+		landingTextBg.classList.remove("hidden");
+	}, 1000);
+}
+
+function backToIndex(){
+	spin.classList.remove("hidden");
+	bodyContainer.classList.add("hidden");
+	landingText.classList.add("hidden");
+	landingTextBg.classList.add("hidden");
+	setTimeout(() => {
+		window.location.href = '../../index.html';
+	}, 1000);
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+	loadIn();
+});
