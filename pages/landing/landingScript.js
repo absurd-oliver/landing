893183c3bf090fe0927f220b80012ce172bced32
absurdoliver <166875516+absurd-oliver@users.noreply.github.com>
2025-09-15 22:38:22 -0400

    Update landingScript.js

--- pages/landing/landingScript.js
+++ pages/landing/landingScript.js
@@ -3,7 +3,6 @@ var clock = 0;
 const spin = document.getElementById("spinner");
 const bodyContainer = document.getElementById("bodyContainer");
 const landingText = document.getElementById("landingText");
-const landingTextBg = document.getElementById("landingTextBg");
 const redirectButtonsContainer = document.getElementById("redirectButtonsContainer");
 
 function loadIn(){
@@ -11,7 +10,6 @@ function loadIn(){
 		spin.classList.add("hidden");
 		bodyContainer.classList.remove("hidden");
 		landingText.classList.remove("hidden");
-		landingTextBg.classList.remove("hidden");
 		redirectButtonsContainer.classList.remove("hidden");
 	}, 1000);
 }
@@ -20,7 +18,6 @@ function backToIndex(){
 	spin.classList.remove("hidden");
 	bodyContainer.classList.add("hidden");
 	landingText.classList.add("hidden");
-	landingTextBg.classList.add("hidden");
 	redirectButtonsContainer.classList.add("hidden");
 	setTimeout(() => {
 		window.location.href = "../../index.html";
@@ -31,7 +28,6 @@ function redirect(where){
 	spin.classList.remove("hidden");
 	bodyContainer.classList.add("hidden");
 	landingText.classList.add("hidden");
-	landingTextBg.classList.add("hidden");
 	redirectButtonsContainer.classList.add("hidden");
 	setInterval(() => {
 		window.location.href = 'https://' + where;
